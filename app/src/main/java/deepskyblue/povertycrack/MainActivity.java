00d213195104f219fb07214d255d86871259281f
Charbar007 <charleymbickel@gmail.com>
2018-03-17 09:50:17 -0600

    3/17/ 03

--- app/src/main/java/deepskyblue/povertycrack/MainActivity.java
+++ app/src/main/java/deepskyblue/povertycrack/MainActivity.java
@@ -2,6 +2,9 @@ package deepskyblue.povertycrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,78 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView Q = findViewById(R.id.questionBox);
+        String test = "how many people were on food stamps in 2015?";
+        Q.setText(test);
+        String answer1 = "100,000";
+        String answer2 = "200,000";
+        String answer3 = "300,000";
+        String answer4 = "400,000";
+        Button choice1 = findViewById(R.id.option1);
+        choice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String response = "you couldn't be more wrong";
+                Q.setText(response);
+            }
+        });
+        choice1.setText(answer1);
+        Button choice2 = findViewById(R.id.option2);
+        choice2.setText(answer2);
+        choice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String response = "Just roll over please";
+                Q.setText(response);
+            }
+        });
+        Button choice3 = findViewById(R.id.option3);
+        choice3.setText(answer3);
+        choice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String response = "Wrong again";
+                Q.setText(response);
+            }
+        });
+        Button choice4 = findViewById(R.id.option4);
+        choice4.setText(answer4);
+        choice4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String response = "It's even more than that";
+                Q.setText(response);
+            }
+        });
+    }
+
+    private void startSolo(){
+
+    }
+
+    private void highLow(String Question, String Answer){
+        TextView Q = findViewById(R.id.questionBox);
+        String test = "how many people were on food stamps in 2015?";
+        Q.setText(test);
+
+    }
+
+    private void checkAll(String Question, String Answer){
+        TextView Q = findViewById(R.id.questionBox);
+        String Answers [] = Answer.split(";");
+        Button choice1 = findViewById(R.id.option1);
+        Button choice2 = findViewById(R.id.option2);
+        Button choice3 = findViewById(R.id.option3);
+        Button choice4 = findViewById(R.id.option4);
+
+    }
+
+    private void multipleChoice(String Question, String Answer){
+        
+
+    }
+
+    private void slider(String Questions){
+
     }
 }
