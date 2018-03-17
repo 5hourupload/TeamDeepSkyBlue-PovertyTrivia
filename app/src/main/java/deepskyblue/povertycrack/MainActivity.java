cc5e708b4a037ad12b263d413a053e257c400ef5
Xeather <34638485+Xeather@users.noreply.github.com>
2018-03-17 10:01:47 -0600

    Updating

--- app/src/main/java/deepskyblue/povertycrack/MainActivity.java
+++ app/src/main/java/deepskyblue/povertycrack/MainActivity.java
@@ -1,5 +1,6 @@
 package deepskyblue.povertycrack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,4 +13,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Intent myIntent = new Intent()
 }
