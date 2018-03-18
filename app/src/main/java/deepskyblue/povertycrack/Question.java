d84d5d82f0f2ac435ada9aff17006e9e31d643a6
Charbar007 <charleymbickel@gmail.com>
2018-03-18 09:47:43 -0600

    push

--- app/src/main/java/deepskyblue/povertycrack/Question.java
+++ app/src/main/java/deepskyblue/povertycrack/Question.java
@@ -1,7 +1,6 @@
 package deepskyblue.povertycrack;
 
 /**
- * Created by Charley Bickel on 3/17/2018.
  * This class is used to instantiate questions called by the solo game method
  * It creates objects with member variables that correspond to the question attributes
  */
