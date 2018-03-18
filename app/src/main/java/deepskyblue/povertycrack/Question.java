b9dd1d23899e86c6e798514489856dc446730626
vvalverde00 <37382698+vvalverde00@users.noreply.github.com>
2018-03-18 09:47:20 -0600

    asdf

--- app/src/main/java/deepskyblue/povertycrack/Question.java
+++ app/src/main/java/deepskyblue/povertycrack/Question.java
@@ -7,32 +7,23 @@ package deepskyblue.povertycrack;
  */
 
 public class Question {
-<<<<<<< HEAD
-    protected int num;
-    protected String question;
-    protected String answer;
-    protected String completeFact;
-    protected boolean correct;
-
-    public Question(int num, String question, String answer, String completeFact, boolean correct){
-        /*All the instantiations are set from the class to later formulate the questions for the
-        app, taking in specific words and data that will be passed on to make a comprehensible
-        question.*/
-=======
     public int num;
     public String question;
     public String answer;
     public String type;
     public String completeFact;
     public boolean correct;
-
-    public Question(int num, String question, String type, String answer, String completeFact, boolean correct){
->>>>>>> 1995ba6489f62c23a24f5caadbdab306f3c09a2f
-        this.num = num;
-        this.question = question;
-        this.type = type;
-        this.answer = answer;
-        this.completeFact = completeFact;
-        this.correct = correct;
+    
+    public Question( int num, String question, String type, String answer, String completeFact,
+        boolean correct){
+        /*All the instantiations are set from the class to later formulate the questions for the
+        app, taking in specific words and data that will be passed on to make a comprehensible
+        question.*/
+            this.num = num;
+            this.question = question;
+            this.type = type;
+            this.answer = answer;
+            this.completeFact = completeFact;
+            this.correct = correct;
+        }
     }
-}
